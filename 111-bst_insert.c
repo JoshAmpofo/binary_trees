af36ee501aa9992ccf63129084a438a3608360e3
JoshAmpofo <joshampofo92@gmail.com>
2023-03-29 14:48:42 +0000

    Insert value into a BST

---
+++ 111-bst_insert.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - inserts a value in a BST.
+ * @tree: double pointer to the root node of the BST to insert the value.
+ * @value: value to store in the node to be inserted.
+ *
+ * Return: pointer to the created node. NULL on failure
+ *
+ * Description: if the address stored in tree is NULL,
+ *              created node must become the root node.
+ *              if value is already present in the tree, it must be ignored.
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *aux = NULL;
+
+	if (tree == NULL)
+		return (NULL);
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+	aux = *tree;
+	if (value < aux->n)
+	{
+		if (aux->left == NULL)
+		{
+			aux->left = binary_tree_node(aux, value);
+			return (aux->left);
+		}
+		return (bst_insert(&(aux->left), value));
+	}
+	if (value > aux->n)
+	{
+		if (aux->right == NULL)
+		{
+			aux->right = binary_tree_node(aux, value);
+			return (aux->right);
+		}
+		return (bst_insert(&(aux->right), value));
+	}
+	return (NULL);
+}
